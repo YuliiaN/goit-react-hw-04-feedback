faf81d34d78f64d3ef5b42c91841ce153c0d05cb
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-15 23:30:41 +0200

    change const name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,62 +4,11 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrement = mark => {
-//     this.setState(prevState => ({
-//       [mark]: prevState[mark] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () =>
-//     this.state.good + this.state.neutral + this.state.bad;
-
-//   countPositiveFeedbackPercentage = () => {
-//     const total = this.countTotalFeedback();
-//     const percentage = Math.round((this.state.good / total) * 100) || 0;
-//     return percentage;
-//   };
-
-//   render() {
-//     const total = this.countTotalFeedback();
-
-//     return (
-//       <>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.handleIncrement}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {total ? (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={total}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
-
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const states = ['good', 'neutral', 'bad'];
+  const reviews = ['good', 'neutral', 'bad'];
 
   const handleIncrement = mark => {
     switch (mark) {
@@ -91,7 +40,7 @@ export const App = () => {
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions options={states} onLeaveFeedback={handleIncrement} />
+        <FeedbackOptions options={reviews} onLeaveFeedback={handleIncrement} />
       </Section>
       <Section title="Statistics">
         {total ? (
