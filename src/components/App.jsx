91f8c51a37b198b62c6e72fa1579251c1f31f307
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-16 14:26:59 +0200

    refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const reviews = ['good', 'neutral', 'bad'];
+  const reviews = Object.keys({ good, neutral, bad });
 
   const handleIncrement = mark => {
     switch (mark) {
